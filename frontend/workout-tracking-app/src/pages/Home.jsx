8bdb11390277f8b25d37355070e6bdf14cfea076
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-13 11:46:57 +0500

    Adding workout

--- frontend/workout-tracking-app/src/pages/Home.jsx
+++ frontend/workout-tracking-app/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
-import WorkoutDetails from "../components/WorkoutDetails";
 
 // components
-// import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
@@ -27,6 +27,7 @@ const Home = () => {
 				<WorkoutDetails workout={workout} key={workout._id} />
             ))}
           </div>
+		  <WorkoutForm />
         </div>
     )
 }
