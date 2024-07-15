1d77bd0a1a0175771ef0acf37b621b9fb994fbd1
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-15 19:50:35 +0500

    Finishing touch

--- frontend/workout-tracking-app/src/components/WorkoutForm.jsx
+++ frontend/workout-tracking-app/src/components/WorkoutForm.jsx
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -26,6 +27,7 @@ const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
             setTitle('')
@@ -46,6 +48,7 @@ const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
 
             <label>Load (in kg):</label>
@@ -53,6 +56,7 @@ const WorkoutForm = () => {
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}    
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps:</label>  
@@ -60,6 +64,7 @@ const WorkoutForm = () => {
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button>Add Workout</button>
